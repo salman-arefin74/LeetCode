45dad576698f2a010a1aae13ca5744c47f39d620
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2022-04-16 16:38:26 +0000

    Solved https://leetcode.com/problems/find-the-difference-of-two-arrays/

---
+++ find-the-difference-of-two-arrays.js
@@ -0,0 +1,25 @@
+var findDifference = function(nums1, nums2) {
+  var num1UniqueElements = [... new Set(nums1)];
+  var num2UniqueElements = [... new Set(nums2)];
+
+  var num1results = [];
+  var num2results = [];
+
+  for(var i=0; i<num1UniqueElements.length; i++){
+    if(!num2UniqueElements.includes(num1UniqueElements[i])){
+      num1results.push(num1UniqueElements[i]);
+    }
+  }
+  
+  for(var i=0; i<num2UniqueElements.length; i++){
+    if(!num1UniqueElements.includes(num2UniqueElements[i])){
+      num2results.push(num2UniqueElements[i]);
+    }
+  }
+  
+  let answer = [];
+  answer.push(num1results);
+  answer.push(num2results);
+  
+  return answer;
+};
--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-var myStringArray = ["flower","flow", "flaod"];
-myStringArray.sort();
-var result = myStringArray[0];
-var arrayLength = myStringArray.length;
-for(var i = 1; i < arrayLength; i++) {
-    for(var j = 0; j < myStringArray[i].length; j++){
-      if(result[j] == myStringArray[i][j]){
-        continue;
-      }
-      else{
-        result = result.substring(0,j);
-        break;
-      }
-    }
+var nums1 = [1,2,3]; 
+var nums2 = [2,4,6];
+
+var num1UniqueElements = [... new Set(nums1)];
+var num2UniqueElements = [... new Set(nums2)];
+
+var num1results = [];
+var num2results = [];
+
+for(var i=0; i<num1UniqueElements.length; i++){
+  if(!num2UniqueElements.includes(num1UniqueElements[i])){
+    num1results.push(num1UniqueElements[i]);
+  }
 }
 
-console.log(result);
+for(var i=0; i<num2UniqueElements.length; i++){
+  if(!num1UniqueElements.includes(num2UniqueElements[i])){
+    num2results.push(num2UniqueElements[i]);
+  }
+}
+
+let answer = [];
+answer.push(num1results);
+answer.push(num2results);
+
+console.log(answer);
