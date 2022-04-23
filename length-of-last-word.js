6dea5dac118ec481b9876e01894a03b45eb03dc9
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2022-04-23 10:09:51 +0000

    Solved https://leetcode.com/problems/length-of-last-word/

--- length-of-last-word.js
+++ length-of-last-word.js
@@ -1,6 +1,7 @@
 var lengthOfLastWord = function(s) {
     let newString = s.replace(/\s+/g,' ');
     let urlParts = newString.split(' ');
+    
     if(urlParts[urlParts.length-1] === '')
     	return urlParts[urlParts.length-2].length;
     else
