f0b9f40bb4f465f201f7b6915e0339433ba73767
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2022-04-23 10:10:51 +0000

    Solved https://leetcode.com/problems/longest-common-prefix/

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -1,18 +1,17 @@
 var longestCommonPrefix = function(strs) {
     strs.sort();
-var result = strs[0];
-var arrayLength = strs.length;
-for(var i = 1; i < arrayLength; i++) {
-    for(var j = 0; j < strs[i].length; j++){
-      if(result[j] == strs[i][j]){
-        continue;
-      }
-      else{
-        result = result.substring(0,j);
-        break;
-      }
+    var result = strs[0];
+    var arrayLength = strs.length;
+    for(var i = 1; i < arrayLength; i++) {
+        for(var j = 0; j < strs[i].length; j++){
+          if(result[j] == strs[i][j]){
+            continue;
+          }
+          else{
+            result = result.substring(0,j);
+            break;
+          }
+        }
     }
-}
-
-return result;
+    return result;
 };
