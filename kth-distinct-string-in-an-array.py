ae03324cd9202d7c9ea175bd1082768223b9e034
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2022-11-04 15:51:45 +0000

    Solved https://leetcode.com/problems/kth-distinct-string-in-an-array/

---
+++ kth-distinct-string-in-an-array.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        freq = Counter(arr)
+        for x in arr: 
+            if freq[x] == 1: 
+                k -= 1
+            if k == 0: 
+                return x
+        return ""
